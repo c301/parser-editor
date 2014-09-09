8b4ec4b5f4432e2411b529b25025a422c093d9c3
Anton Sivolapov <anton.sivolapov@gmail.com>
2014-09-09 11:43:27 +0400

    add server start. move to src folder

--- src/app.js
+++ src/app.js
@@ -7,11 +7,20 @@ require([
 ], 
 function ( Compiler, CMS, storage, _ ) {
     "use strict";
+    var APP = {};
 
     console.log('APP start..');
 
     var afterInit = function () {
         console.log('DB was filled.')
+        applyTree('DB was filled.');
     }
     storage.init( afterInit );
+
+    function applyTree(message) {
+        require(['dijit/Dialog'], function(Dialog){
+            var dialog = new Dialog({ title: 'Hello!', content: message });
+            dialog.show();
+        });
+    }
 })
